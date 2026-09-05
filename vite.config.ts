b3f544698fe0d8e8498af7d33bf797e109912ff7
MANG369 <nievesgarciamarcoantonio@gmail.com>
2026-09-05 13:08:28 +0000

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mi-Constitucion/', // Nombre exacto de tu repositorio
+  base: '/Mi-Constitucion/',
 });
